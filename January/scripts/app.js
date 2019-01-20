2be638a7d652f38ea2f83d1ce6818e6fb7dab347
maryzam <zamo.code@gmail.com>
2019-01-20 23:44:55 +0300

    Update styles

--- January/scripts/app.js
+++ January/scripts/app.js
@@ -28,6 +28,20 @@ Promise.all([
 					.append("svg")
 					.attr("width", width)
 					.attr("height", height);
+		//add defs
+		svg
+			.append("defs")
+			.append("marker")
+				.attr("id", "arrow")
+				.attr('markerHeight', 5)
+		        .attr('markerWidth', 5)
+		        .attr('markerUnits', 'strokeWidth')
+		        .attr('orient', 'auto')
+		        .attr('refX', 0)
+		        .attr('refY', 0)
+		    .append("path")
+		    	.attr("d", "M 0,0 m -5,-5 L 5,0 L -5,5 Z")
+		    	.style("fill", "blue");
 
 		//prepare & render world map
 		const projection = d3.geoFahey()
@@ -59,7 +73,6 @@ Promise.all([
         	}
         	const from = projection([d.BirthplaceCoords[1],d.BirthplaceCoords[0]]);
         	const till = projection([d.DeathplaceCoords[1],d.DeathplaceCoords[0]]);
-        	offsets
 
         	return `M ${from[0]} ${from[1]} C ${from[0] - offset} ${from[1] - offset} ${till[0] + offset} ${till[1] - offset} ${till[0]} ${till[1]}`;
         };
@@ -75,6 +88,7 @@ Promise.all([
         	.append("path")
         	.attr("d", lifePath);
 
+        /*
         const starPath = d3.symbol()
         					.type(d3.symbolStar)
         					.size(30);
@@ -85,6 +99,6 @@ Promise.all([
         		.attr("transform", (d) => {
         			const till = projection([d.DeathplaceCoords[1],d.DeathplaceCoords[0]]);
         			return `translate(${till[0]} ${till[1]})`;
-        		});
+        		}); */
 
 	}).catch((e) => console.error(error))
